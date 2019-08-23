86a0141edc9cf2f3361543af5f3ea4f22ae814f7
unknown <sachinswapna143@gmail.com>
2019-08-23 07:07:18 +0530

    first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as echarts from 'echarts';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'api';
+  data;
+  @ViewChild('echart1',{static:true}) main:ElementRef;
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    // this.getAggregateData();
+    this.getData();
+  }
+
+  async getData(){
+    this.data = await (this.http.get("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI").toPromise());
+    console.table(this.data)
+  }
+  async getAggregateData(){
+    this.data = await (this.http.get("http://localhost:7000/chat/group/messages").toPromise());
+    console.log(this.data)
+    // console.log(this.main)
+    var myChart = echarts.init(this.main.nativeElement);
+    // specify chart configuration item and data
+    console.log(myChart)
+    var option = {
+        title: {
+            text: 'ECharts entry example'
+        },
+        tooltip: {},
+        legend: {
+            data:['Messages_Count']
+        },
+        xAxis: {
+            data: this.data.map(d=>d["_id"])
+        },
+        yAxis: {},
+        series: [{
+            name: 'Messages_Count',
+            type: 'bar',
+            data: this.data.map(d=>d["count"])
+        }]
+    };
+
+    // use configuration item and data specified to show chart
+    myChart.setOption(option);
+    // console.log(myChart)
+    Highcharts.chart("main",{
+      chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Fruit Consumption'
+    },
+    xAxis: {
+        categories: this.data.map(d=>d["_id"])
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit eaten'
+        }
+    },
+    series: [{
+        name: 'Messages Count',
+        data: this.data.map(d=>d["count"]),
+        type:undefined
+      }]
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './users/users.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
